a5235344defadf036cc1c1099326614726381b09
shren207 <shren0812@gmail.com>
2022-04-16 23:54:33 +0900

    1. 캔버스 생성과 그리기[수정]

--- app.ts
+++ app.ts
@@ -37,3 +37,9 @@ ctx.arc(240, 160, 20, 0, Math.PI * 2, false); // 원은 arc()
 ctx.fillStyle = "green"; // 칠할 색깔
 ctx.fill(); // 칠하기
 ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
+ctx.closePath();
